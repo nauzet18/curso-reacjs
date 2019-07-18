c1353f62982181e64e5f6daf3e6beea6d772ce5f
Nauzet Melián Batista <nmelian@usabi.es>
2019-07-18 22:32:35 +0100

    Como se agrega un fichero CSS  y se usa una clase

--- tiempoapp/src/components/TiempoData.js
+++ tiempoapp/src/components/TiempoData.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import TiempoExtraInfo from './TiempoExtraInfo'
 import TiempoTemperatura from './TiempoTemperatura'
+import './style.css'
 
 const TiempoData = () => (
-    <div>
+    <div className="tiempoDataCont">
         <TiempoTemperatura temp={24.4} state={'cloudy'}></TiempoTemperatura>
         <TiempoExtraInfo humedad={80} viento={'50 km/h'}></TiempoExtraInfo>
     </div>
